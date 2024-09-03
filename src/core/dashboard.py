d4a8011f33b08811a52c48a5abdfe5f8a7a2adff
SoftZest Studio <contact.softzeststudio@gmail.com>
2024-09-04 05:01:06 +0530

    Update dashboard.py

--- src/core/dashboard.py
+++ src/core/dashboard.py
@@ -1,17 +1,10 @@
-# src/core/dashboard.py
-
-import curses
 from core.api_manager import APIManager
-from core.authentication import AuthenticationManager
-from platforms.twitter import TwitterAPI
-from platforms.reddit import RedditAPI
-from platforms.mastodon import MastodonAPI
-
 
 class SocialMediaDashboard:
-    def __init__(self, config):
-        self.api_manager = APIManager(config)
-        self.tui_manager = TUIManager(config)
+    def __init__(self, credentials):
+        self.api_manager = APIManager(credentials)
 
-    def run(self):
-        curses.wrapper(self.tui_manager.display_dashboard, self.api_manager)
+    def post_to_all(self, message):
+        self.api_manager.post_to_twitter(message)
+        self.api_manager.post_to_reddit(message)
+        self.api_manager.post_to_mastodon(message)
